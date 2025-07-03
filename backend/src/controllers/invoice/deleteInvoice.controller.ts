f6a9806b842f6163b8bbe34daa9029ead3ecbc70
karansinghcodes <karansinghcodes@gmail.com>
2025-07-03 17:57:05 +0530

    fixed error

--- backend/src/controllers/invoice/deleteInvoice.controller.ts
+++ backend/src/controllers/invoice/deleteInvoice.controller.ts
@@ -25,11 +25,21 @@ export const deleteInvoice = async (req: Request, res: Response) => {
                 }
             })
 
+            await prisma.client.update({
+                where: {
+                    id: invoice.clientId
+                },
+                data: {
+                    invoiceCount: { decrement: 1 },
+                    totalBilledAmount: { decrement: invoice.grandTotal }
+                }
+            })
+
             response.ok(res, "Invoice Deleted Successfully", 200);
             return
         }
-        else{
-            response.error(res,"No Invoice Exists With This Id",404);
+        else {
+            response.error(res, "No Invoice Exists With This Id", 404);
             return
         }
 
@@ -42,4 +52,4 @@ export const deleteInvoice = async (req: Request, res: Response) => {
     }
 }
 
-api.delete("/delete-invoice/:invoiceId","protected",deleteInvoice);
+api.delete("/delete-invoice/:invoiceId", "protected", deleteInvoice);
