a9915b7bc7541f4c8887dda19e136317721290de
karansinghcodes <karansinghcodes@gmail.com>
2025-07-12 17:38:21 +0530

    refactoring code for error handeling

--- frontend/src/pages/auth/SignUp.tsx
+++ frontend/src/pages/auth/SignUp.tsx
@@ -1,184 +1,213 @@
 import { FileText } from "lucide-react";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useForm, type SubmitHandler } from "react-hook-form"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { signUpSchema } from "@/validations/user/signUpSchema";
 import z from "zod";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import BasicLoader from "@/components/custom/BasicLoader";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import { apiCall } from "@/utils/api/apiCall";
-
-
-
+import { useMutation } from "@tanstack/react-query";
+import type { UserSignupFormData } from "@/types/user";
 
 const SignUp = () => {
-    let navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [loading, setLoading] = useState<boolean>(false);
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (formData: UserSignupFormData) => {
+      return await apiCall<null, UserSignupFormData>(
+        "/sign-up",
+        "POST",
+        "noauth",
+        formData
+      );
+    },
+  });
 
-    const { register, handleSubmit, formState: { errors } } = useForm<z.infer<typeof signUpSchema>>({
-        resolver: zodResolver(signUpSchema),
-        defaultValues: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
 
-        },
-    });
-
-    const onSubmit: SubmitHandler<z.infer<typeof signUpSchema>> = async (data) => {
-
-        try {
-            setLoading(true);
-            const result = await apiCall<null>("/sign-up", "POST", "noauth", data);
-
-            if (result.success) {
-                toast.success(result.message);
-                navigate(`/verify-code/${data.email}`)
-            }
-            else {
-                toast.error(result.message);
-
-
-            }
-        } catch (error) {
-            console.error("api call failed");
-
-
-        } finally {
-            setLoading(false);
+  const onSubmit: SubmitHandler<UserSignupFormData> = async (formData) => {
+    mutate(formData, {
+      onSuccess: (response) => {
+        if (response.success) {
+          toast.success(response.message);
+          navigate(`/verify-code/${formData.email}`);
+        } else {
+          toast.error(response.message || "Something went wrong");
         }
+      },
+      onError: (error: any) => {
+        console.error("Registration failed:", error.message);
+        toast.error("Registration failed: " + error.message);
+      },
+    });
+  };
 
-
-
-
-    }
-
-    return (
-        <div className="bg-slate-50 min-h-screen bg-slate-50 flex flex-col items-center gap-8 justify-center">
-
-
-            <div className="flex items-center gap-2 ">
-                <div className="bg-emerald-500 rounded-md p-2 flex items-center jusify-center">
-                    <FileText className="h-8 w-8 text-white" />
-                </div>
-                <h1 className="text-2xl text-slate-900 font-bold">InvoDrop</h1>
-            </div>
-
-            {/* //signup card  */}
-
-            <Card className="w-120 shadow-xl border-none">
-                <CardHeader className="text-center">
-                    <CardTitle className="text-2xl font-bold text-center text-slate-900">Welcome </CardTitle>
-                    <CardDescription className="text-center text-slate-600">
-                        Enter your deatails  to continue
-                    </CardDescription>
-
-                </CardHeader>
-                <CardContent>
-                    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)} >
-                        <div className="flex flex-col space-y-6">
-                            <div className="flex items-center gap-5">
-                                <div>
-                                    <Label htmlFor="email" className="font-medium text-sm text-slate-700 mb-1">First Name</Label>
-                                    <Input
-                                        {...register("firstName")}
-                                        id="firstName"
-                                        type="firstName"
-                                        placeholder="Shilendra"
-                                        className="h-11 border-slate-200 placeholder:text-slate-700  focus-visible:ring-emerald-500"
-                                        required
-                                    />
-                                    {errors.firstName && (
-                                        <p className="text-sm text-rose-500 mt-1">
-                                            {errors.firstName.message}
-                                        </p>
-
-                                    )}
-
-                                </div>
-                                <div>
-                                    <Label htmlFor="lastName" className="font-medium text-sm text-slate-700 mb-1">Last Name</Label>
-                                    <Input
-                                        {...register("lastName")}
-                                        id="lastName"
-                                        type="lastName"
-                                        placeholder="Singh"
-                                        className="h-11 border-slate-200 placeholder:text-slate-700 focus-visible:ring-emerald-500"
-                                        required
-                                    />
-                                    {errors.lastName && (
-                                        <p className="text-sm text-rose-500 mt-1">
-                                            {errors.lastName.message}
-                                        </p>
-
-                                    )}
-                                </div>
-                            </div>
-
-                            <div>
-                                <Label htmlFor="email" className="text-sm font-medium text-slate-700 mb-1">
-                                    Email address
-                                </Label>
-                                <Input
-                                    {...register("email")}
-                                    id="email"
-                                    type="email"
-                                    placeholder="example@example.com"
-                                    className="h-11 border-slate-200 placeholder:text-slate-700 focus-visible:ring-emerald-500"
-
-                                    required
-                                />
-                                {errors.email && (
-                                    <p className="text-sm text-rose-500 mt-1">
-                                        {errors.email.message}
-                                    </p>
-
-                                )}
-                            </div>
-                            <div>
-                                <div className="flex items-center">
-                                    <Label htmlFor="password" className="text-sm font-medium text-slate-700 mb-1">Password</Label>
-
-                                </div>
-                                <Input id="password" type="password"
-                                    {...register("password")}
-                                    className="h-11 border-slate-200 focus-visible:ring-emerald-500 placeholder:text-slate-700"
-                                    placeholder="Enter your password"
-                                    required />
-                                {errors.password && (
-                                    <p className="text-sm text-rose-500 mt-1">
-                                        {errors.password.message}
-                                    </p>
-
-                                )}
-                            </div>
-                            <Button type="submit" disabled={loading} className="w-full bg-emerald-600 hover:bg-emerald-700">
-                                {loading ? <BasicLoader /> : <p>Sign Up</p>}
-                            </Button>
-                        </div>
-
-                    </form>
-                </CardContent>
-
-            </Card>
-            <p className="text-slate-700 text-sm">By signing up, you agree to our <a href="term-of-service" className="text-emerald-600 hover:text-emerald-700"> Terms of Service</a> and <a href="/privacy-policy" className="text-emerald-600 hover:text-emerald-700"> Privacy Policy</a></p>
-
+  return (
+    <div className="bg-slate-50 min-h-screen bg-slate-50 flex flex-col items-center gap-8 justify-center">
+      <div className="flex items-center gap-2 ">
+        <div className="bg-emerald-500 rounded-md p-2 flex items-center jusify-center">
+          <FileText className="h-8 w-8 text-white" />
         </div>
-    )
-}
+        <h1 className="text-2xl text-slate-900 font-bold">InvoDrop</h1>
+      </div>
 
+      {/* //signup card  */}
+
+      <Card className="w-120 shadow-xl border-none">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold text-center text-slate-900">
+            Welcome{" "}
+          </CardTitle>
+          <CardDescription className="text-center text-slate-600">
+            Enter your deatails to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col space-y-6">
+              <div className="flex items-center gap-5">
+                <div>
+                  <Label
+                    htmlFor="email"
+                    className="font-medium text-sm text-slate-700 mb-1"
+                  >
+                    First Name
+                  </Label>
+                  <Input
+                    {...register("firstName")}
+                    id="firstName"
+                    type="firstName"
+                    placeholder="First Name"
+                    className="h-11 border-slate-200 placeholder:text-slate-700  focus-visible:ring-emerald-500"
+                    required
+                  />
+                  {errors.firstName && (
+                    <p className="text-sm text-rose-500 mt-1">
+                      {errors.firstName.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <Label
+                    htmlFor="lastName"
+                    className="font-medium text-sm text-slate-700 mb-1"
+                  >
+                    Last Name
+                  </Label>
+                  <Input
+                    {...register("lastName")}
+                    id="lastName"
+                    type="lastName"
+                    placeholder="Last Name"
+                    className="h-11 border-slate-200 placeholder:text-slate-700 focus-visible:ring-emerald-500"
+                    required
+                  />
+                  {errors.lastName && (
+                    <p className="text-sm text-rose-500 mt-1">
+                      {errors.lastName.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <div>
+                <Label
+                  htmlFor="email"
+                  className="text-sm font-medium text-slate-700 mb-1"
+                >
+                  Email address
+                </Label>
+                <Input
+                  {...register("email")}
+                  id="email"
+                  type="email"
+                  placeholder="example@gmail.com"
+                  className="h-11 border-slate-200 placeholder:text-slate-700 focus-visible:ring-emerald-500"
+                  required
+                />
+                {errors.email && (
+                  <p className="text-sm text-rose-500 mt-1">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <div className="flex items-center">
+                  <Label
+                    htmlFor="password"
+                    className="text-sm font-medium text-slate-700 mb-1"
+                  >
+                    Password
+                  </Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                  className="h-11 border-slate-200 focus-visible:ring-emerald-500 placeholder:text-slate-700"
+                  placeholder="Enter your password"
+                  required
+                />
+                {errors.password && (
+                  <p className="text-sm text-rose-500 mt-1">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+              <Button
+                type="submit"
+                disabled={isPending}
+                className="w-full bg-emerald-600 hover:bg-emerald-700"
+              >
+                {isPending ? <BasicLoader /> : <p>Sign Up</p>}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <p className="text-slate-700 text-sm">
+        By signing up, you agree to our{" "}
+        <a
+          href="term-of-service"
+          className="text-emerald-600 hover:text-emerald-700"
+        >
+          {" "}
+          Terms of Service
+        </a>{" "}
+        and{" "}
+        <a
+          href="/privacy-policy"
+          className="text-emerald-600 hover:text-emerald-700"
+        >
+          {" "}
+          Privacy Policy
+        </a>
+      </p>
+    </div>
+  );
+};
 
 export default SignUp;
--- frontend/src/pages/auth/VerifyCode.tsx
+++ frontend/src/pages/auth/VerifyCode.tsx
@@ -1,110 +1,121 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useForm, type SubmitHandler } from "react-hook-form"
-import z from "zod";
-import { zodResolver } from '@hookform/resolvers/zod';
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { apiCall } from "@/utils/api/apiCall";
-import { useState } from "react";
 import BasicLoader from "@/components/custom/BasicLoader";
 import { toast } from "sonner";
 import { verificationCodeSchema } from "@/validations/user/verificationCodeSchema";
 import { useNavigate, useParams } from "react-router-dom";
-
-
+import { useMutation } from "@tanstack/react-query";
+import type { UserVerificationData } from "@/types/user";
 
 const VerifyCode = () => {
+  const navigate = useNavigate();
 
-    let navigate = useNavigate();
+  const { emailAddress } = useParams();
 
-    const { emailAddress } = useParams();
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (formData: UserVerificationData) => {
+      return await apiCall<null, UserVerificationData>(
+        "/verify-code",
+        "POST",
+        "noauth",
+        formData
+      );
+    },
+  });
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserVerificationData>({
+    resolver: zodResolver(verificationCodeSchema),
+    defaultValues: {
+      email: emailAddress,
+      verifyCode: "",
+    },
+  });
 
-    const [loading, setLoading] = useState<boolean>(false);
-
-    const { register, handleSubmit, formState: { errors } } = useForm<z.infer<typeof verificationCodeSchema>>({
-        resolver: zodResolver(verificationCodeSchema),
-        defaultValues: {
-            email: emailAddress,
-            verifyCode: ""
-
-        },
+  const onSubmit: SubmitHandler<UserVerificationData> = async (formData) => {
+    mutate(formData, {
+      onSuccess: (response) => {
+        if (response.success) {
+          toast.success(response.message);
+          navigate(`/login`);
+        } else {
+          toast.error(response.message || "Something went wrong");
+        }
+      },
+      onError: (error: any) => {
+        console.error("Registration failed:", error.message);
+        toast.error("Registration failed: " + error.message);
+      },
     });
+  };
 
-    const onSubmit: SubmitHandler<z.infer<typeof verificationCodeSchema>> = async (data) => {
+  return (
+    <div className="bg-slate-50 min-h-screen bg-slate-50 flex flex-col items-center gap-8 justify-center">
+      {/* //Verification Code card  */}
 
-        setLoading(true);
-
-        const result= await apiCall<null>("/verify-code", "POST", "noauth", data);
-
-        if (result.success) {
-            toast.success(result.message);
-            setLoading(false);
-            navigate("/login")
-        }
-        else {
-            toast.error(result.message);
-            setLoading(false);
-        }
-        setLoading(true);
-
-    }
-
-    return (
-        <div className="bg-slate-50 min-h-screen bg-slate-50 flex flex-col items-center gap-8 justify-center">
-
-            {/* //Verification Code card  */}
-
-            <Card className="w-120 shadow-xl border-none ">
-                <CardHeader className="text-center">
-                    <CardTitle className="text-2xl font-bold text-center text-slate-900">Verify Code </CardTitle>
-                    <CardDescription className="text-center text-slate-600 mt-1">
-                        Enter the verifycode we sent to your email {emailAddress}
-                    </CardDescription>
-
-                </CardHeader>
-                <CardContent>
-                    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)} >
-                        <div className="flex flex-col space-y-6">
-
-                            <div>
-                                <div className="flex items-center">
-                                    <Label htmlFor="verifyCode" className="text-sm font-medium text-slate-700 mb-1">Verification Code</Label>
-
-                                </div>
-                                <Input id="verifyCode" type="text"
-                                    {...register("verifyCode")}
-                                    className="h-11 border-slate-200 focus-visible:ring-emerald-500 placeholder:text-slate-700"
-                                    placeholder="Enter the 6 digit verify code"
-                                    required />
-                                {errors.verifyCode && (
-                                    <p className="text-sm text-rose-500 mt-1">
-                                        {errors.verifyCode.message}
-                                    </p>
-
-                                )}
-                            </div>
-                            <Button type="submit" disabled={loading} className="w-full bg-emerald-600 hover:bg-emerald-700">
-                                {loading ? <BasicLoader /> : <p>Verify Code</p>}
-                            </Button>
-                        </div>
-
-                    </form>
-                </CardContent>
-
-            </Card>
-
-
-        </div>
-    )
-}
-
+      <Card className="w-120 shadow-xl border-none ">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold text-center text-slate-900">
+            Verify Code{" "}
+          </CardTitle>
+          <CardDescription className="text-center text-slate-600 mt-1">
+            Enter the verifycode we sent to your email{" "}
+            <span className="font-semibold underline">{emailAddress}</span>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col space-y-6">
+              <div>
+                <div className="flex items-center">
+                  <Label
+                    htmlFor="verifyCode"
+                    className="text-sm font-medium text-slate-700 mb-1"
+                  >
+                    Verification Code
+                  </Label>
+                </div>
+                <Input
+                  id="verifyCode"
+                  type="text"
+                  {...register("verifyCode")}
+                  className="h-11 border-slate-200 focus-visible:ring-emerald-500 placeholder:text-slate-700"
+                  placeholder="Enter the 6 digit verify code"
+                  required
+                />
+                {errors.verifyCode && (
+                  <p className="text-sm text-rose-500 mt-1">
+                    {errors.verifyCode.message}
+                  </p>
+                )}
+              </div>
+              <Button
+                type="submit"
+                disabled={isPending}
+                className="w-full bg-emerald-600 hover:bg-emerald-700"
+              >
+                {isPending ? <BasicLoader /> : <p>Verify Code</p>}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export default VerifyCode;
