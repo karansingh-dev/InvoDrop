bb0f07225b9413fc56032a65c306b85b0530d061
karansinghcodes <karansinghcodes@gmail.com>
2025-07-16 16:01:36 +0530

    refactored code

--- backend/src/controllers/client/addClient.controller.ts
+++ backend/src/controllers/client/addClient.controller.ts
@@ -2,45 +2,58 @@ import { Request, Response } from "express";
 import { addClientSchema } from "../../validations/client/addClientSchema";
 import { response } from "../../utils/response";
 import prisma from "../../helpers/prismaClient";
-import z from "zod";
 import { api } from "../../routes/router";
-import { customRequest } from "../../types/customRequest";
-
-type client = z.infer<typeof addClientSchema>;
-
-interface newClient extends client {
-  userId: string;
-}
+import { ClientDataType, NewClientDataType } from "../../types/client";
 
 export const addClient = async (req: Request, res: Response) => {
-  const client: client = req.body;
-  const user: customRequest = req.user;
+  try {
+    const client: ClientDataType = req.body;
+    const user = req.user;
 
-  const requestValidation = addClientSchema.safeParse(client);
+    const requestValidation = addClientSchema.safeParse(client);
 
-  if (requestValidation.success) {
-    const clientExist = await prisma.client.findFirst({
-      where: {
-        email: client.email,
-        userId: user.userId,
-      },
-    });
-    if (clientExist) {
-      return response.error(res, "Client Already Exists With This Email", 409);
-      
-    } else {
-      const newClient: newClient = JSON.parse(JSON.stringify(client));
-
-      newClient.userId = user.userId;
-
-      await prisma.client.create({
-        data: newClient,
-      });
-
-      return response.ok(res, "Client Added Successfully", 201);
+    if (!requestValidation) {
+      return response.error(res, "Invalid Data Sent", 400);
     }
-  } else {
-    return response.error(res, "Invalid Data Sent", 400);
+
+    let clientExist;
+
+    try {
+      clientExist = await prisma.client.findUnique({
+        where: {
+          email_userId: {
+            email: client.email,
+            userId: user.userId,
+          },
+        },
+      });
+    } catch (error) {
+      console.log("DB: failed to Get Client", error);
+      throw error;
+    }
+
+    if (clientExist) {
+      return response.error(res, "Client Already Exist With This Email ", 409);
+    }
+
+    const addNewClient: NewClientDataType = {
+      ...client,
+      userId: user.userId,
+    };
+
+    try {
+      await prisma.client.create({
+        data: addNewClient,
+      });
+    } catch (error) {
+      console.log("DB:Failed to Add new Client");
+      throw error;
+    }
+
+    return response.ok(res, "Client Added Successfully", 201);
+  } catch (error) {
+    console.log("Failed to Add client", error);
+    throw error;
   }
 };
 
