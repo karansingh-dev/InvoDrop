a5f395a82095414e540ae607d7055eca28148eef
karansinghcodes <karansinghcodes@gmail.com>
2025-07-16 17:34:05 +0530

    successfully refactored Addclient

--- frontend/src/components/custom/Loaders/BasicLoader2.tsx
+++ frontend/src/components/custom/Loaders/BasicLoader2.tsx
@@ -1,9 +1,9 @@
-import { LoaderCircle } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
 export default function BasicLoader2() {
   return (
     <div className="flex flex-col items-center gap-4 text-blue-500 animate-pulse">
-      <LoaderCircle className="w-10 h-10 animate-spin" />
+      <Loader2 className="animate-spin w-12 h-12 text-white" />
     </div>
   );
 }
